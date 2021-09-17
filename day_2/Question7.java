2c93edc58d66d276faaecd9097e7488a94dc1c50
omrfrkn <ofurkandogan@gmail.com>
2021-09-17 17:47:08 +0300

    Assigment1 uploaded

---
+++ day_2/Question7.java
@@ -0,0 +1,12 @@
+package day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		System.out.println("    /\\    /\\  ");
+		System.out.println("   /  \\  /  \\  ");
+		System.out.println("   ----  ----");
+		System.out.println("I am from Germany"+"\nHow is your day going on, 'good' or 'bad' ?");
+		}
+
+}
